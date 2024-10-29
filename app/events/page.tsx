75c1f5c0a6ae32193ab2ba77e8850a1143845111
chekirko <victorche59@gmail.com>
2024-10-29 15:19:50 +0200

    protect routes

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,19 +1,12 @@
-import Link from "next/link";
 import { EventsPageComponent } from "@/components";
+import CreateEventLink from "@/components/CreateEventLink";
 
 export const dynamic = "force-dynamic";
 
 const EventsPage = () => {
   return (
     <div className="padding-x mt-16 max-w-[1600px] mx-auto min-h-screen">
-      <div className="flex justify-end">
-        <Link
-          href="/events/create-new"
-          className="py-3 px-8 bg-primary font-bold text-lg hover:bg-primary-dark text-white rounded-full"
-        >
-          Новий список
-        </Link>
-      </div>
+      <CreateEventLink />
       <EventsPageComponent />
     </div>
   );
